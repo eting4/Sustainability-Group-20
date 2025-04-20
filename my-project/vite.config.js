6cfc00686e5aa67af93a053cc22cca9f00f2c56b
eting4 <128740931+eting4@users.noreply.github.com>
2025-04-20 14:59:32 -0400

    deploy 2

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Sustainability-Group-20/',
   plugins: [react(), tailwindcss()],
 })
